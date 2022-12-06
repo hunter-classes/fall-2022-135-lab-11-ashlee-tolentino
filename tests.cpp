04ef840ef151047f486a30905a2058cd093815eb
Ashlee Tolentino <104875387+ashlee-tolentino@users.noreply.github.com>
2022-12-06 10:31:45 -0500

    Network Class tests done

--- tests.cpp
+++ tests.cpp
@@ -19,8 +19,25 @@ TEST_CASE("Profile Class"){
   CHECK(p2.getFullName() == "Ash (@ash8)");
 }
 
-/*
 TEST_CASE("Network Class"){
-  
+  Network n1;
+  CHECK(n1.addUser("mario", "Mario") == true);
+  CHECK(n1.addUser("luigi", "Luigi") == true);
+  CHECK(n1.addUser("mario", "Mario2") == false);
+  CHECK(n1.addUser("mario 2", "Mario2") == false);
+  CHECK(n1.addUser("mario-2", "Mario2") == false);
+  for(int i = 2; i < 20; i++) {
+    n1.addUser("mario" + std::to_string(i), "Mario" + std::to_string(i));
+  }
+  CHECK(n1.addUser("yoshi", "Yoshi") == false);
+
+  Network n2;
+  n2.addUser("mario", "Mario");
+  n2.addUser("luigi", "Luigi");
+  n2.addUser("yoshi", "Yoshi");
+  CHECK(n2.follow("mario", "luigi") == true);
+  CHECK(n2.follow("yoshi", "luigi") == true);
+  CHECK(n2.follow("wario", "mario") == false);
+  CHECK(n2.follow("luigi", "wario") == false);
 }
-*/
+
